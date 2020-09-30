eb518fe8765aee6d84e127eeb9f5601dda2ab27d
woong9149 <sewoong0409@naver.com>
2020-09-30 11:59:09 +0900

    material-ui 추가 및 아이콘 추가
    material-ui 추가 및 아이콘 추가

--- whatsapp-mern/src/Sidebar.js
+++ whatsapp-mern/src/Sidebar.js
@@ -1,12 +1,36 @@
 import React from 'react';
 import  "./Sidebar.css";
+import DonutLargeIcon from '@material-ui/icons/DonutLarge';
+import { Avatar, IconButton } from "@material-ui/core";
+import ChatIcon from "@material-ui/icons/Chat";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import { SearchOutlined } from "@material-ui/icons";
 
 function Sidebar(){
     return(
         <div className='sidebar'>
-            
-            <h1>I am the sidebar</h1>
-       
+            <div className="sidebar__header">
+                <Avatar />
+                <div className="sidebar__headerRight">
+                    <IconButton>
+                        <DonutLargeIcon />
+                    </IconButton>
+                    <IconButton> 
+                        <ChatIcon />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVertIcon />
+                    </IconButton>
+                </div>
+            </div>
+
+            <div className="sidebar__search">
+                <div className="sidebar__searchContainer">
+                    <SearchOutlined />
+                    <input placeholder="Search or start new chat" type="text"/>
+                </div>
+            </div>
+
         </div>
     )
 }
